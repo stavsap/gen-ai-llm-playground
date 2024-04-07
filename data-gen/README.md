6eba32a3644a6db1aa486e327a698f9594d2ca14
Stav Sapir <stavsap@gmail.com>
2024-04-07 12:13:34 +0300

    add data -gen
    Signed-off-by: Stav Sapir <stavsap@gmail.com>

---
+++ data-gen/README.md
@@ -0,0 +1,14 @@
+
+https://github.com/outlines-dev/outlines
+
+https://github.com/abetlen/llama-cpp-python
+
+set CMAKE_ARGS="-DLLAMA_CUDA=on -DLLAMA_CUBLAS=on"
+
+pip install llama-cpp-python --upgrade --force-reinstall --no-cache-dir
+
+```shell
+pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu121
+pip install llama-cpp-python --upgrade --force-reinstall --no-cache-dir --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/123
+pip install -r requirements.txt
+```
--- data-gen/data-gen.py
+++ data-gen/data-gen.py
@@ -1,12 +1,76 @@
 import outlines
 
-model = outlines.models.transformers("mistralai/Mistral-7B-Instruct-v0.2")
+from transformers import BitsAndBytesConfig
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True,
+)
+
+args ={}
+args["quantization_config"] = bnb_config
+
+
+model = outlines.models.llamacpp(model_path="mistral-7b-instruct-v0.2.Q2_K.gguf", device="cuda")
+# model = outlines.models.transformers("mistralai/Mistral-7B-Instruct-v0.2", device="cuda", model_kwargs=args)
 
 prompt = """You are a sentiment-labelling assistant.
 Is the following review positive or negative?
 
-Review: This restaurant is just awesome!
+Review: This restaurant is just awesome! its was so good, i love it!
 """
 
 generator = outlines.generate.choice(model, ["Positive", "Negative"])
 answer = generator(prompt)
+print(answer)
+
+schema = '''{
+    "title": "Character",
+    "type": "object",
+    "properties": {
+        "name": {
+            "title": "Name",
+            "maxLength": 10,
+            "type": "string"
+        },
+        "age": {
+            "title": "Age",
+            "type": "integer"
+        },
+        "armor": {"$ref": "#/definitions/Armor"},
+        "weapon": {"$ref": "#/definitions/Weapon"},
+        "strength": {
+            "title": "Strength",
+            "type": "integer"
+        }
+    },
+    "required": ["name", "age", "armor", "weapon", "strength"],
+    "definitions": {
+        "Armor": {
+            "title": "Armor",
+            "description": "An enumeration.",
+            "enum": ["leather", "chainmail", "plate"],
+            "type": "string"
+        },
+        "Weapon": {
+            "title": "Weapon",
+            "description": "An enumeration.",
+            "enum": ["sword", "axe", "mace", "spear", "bow", "crossbow"],
+            "type": "string"
+        }
+    }
+}'''
+
+generator = outlines.generate.json(model, schema)
+character = generator("Give me a character description")
+print(character)
+
+character = generator("Give me a character description")
+print(character)
+
+character = generator("Give me a character description")
+print(character)
+
+character = generator("Give me a character description")
+print(character)
